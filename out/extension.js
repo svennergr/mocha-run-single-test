b956f2ffe8f904d848c98b2c7413130cc27420f5
Sven Großmann <sven.grossmann@lhind.dlh.de>
2020-02-07 09:51:49 +0100

    extension now resolves paths to POSIX paths and reopens closed terminals

--- out/extension.js
+++ out/extension.js
@@ -1,40 +1,49 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
+const path = require("path");
 const vscode = require("vscode");
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "test-from-contextmenu" is now active!');
-    // save for later
-    let terminal;
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand("extension.runTest", () => {
-        // The code you place here will be executed every time your command is executed
-        // current editor
-        const editor = vscode.window.activeTextEditor;
-        const fullFile = editor.document.fileName;
-        const path = fullFile.substring(0, fullFile.lastIndexOf("\\") + 1);
-        const fileName = fullFile.replace(path, '');
-        const testFileName = fullFile.replace('\\src\\', '\\build\\');
-        // check if there is no selection
-        const testName = editor.document.getText(editor.selection);
-        if (!terminal) {
-            terminal = vscode.window.createTerminal('Test Terminal');
-        }
-        terminal.show();
-        terminal.sendText(`cd ${path}..\\..`);
-        terminal.sendText(`gulp build; mocha -g '${testName}' ${testFileName} --exit`);
-    });
-    context.subscriptions.push(disposable);
+  let terminal;
+
+  context.subscriptions.push(
+    vscode.window.onDidCloseTerminal(closedTerminal => {
+      if (closedTerminal == terminal) {
+        terminal = null;
+      }
+    })
+  );
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("extension.runTest", () => {
+    // The code you place here will be executed every time your command is executed
+    // current editor
+    const editor = vscode.window.activeTextEditor;
+    const fullFile = editor.document.fileName;
+
+    const filePath = path.dirname(fullFile).replace(/\\/g, "/");
+    // const fileName = path.basename(filePath);
+    const testFileName = fullFile
+      .replace(/\\/g, "/")
+      .replace("/src/", "/build/");
+    // check if there is no selection
+    const testName = editor.document.getText(editor.selection); // TODO: determine this
+
+    if (!terminal) {
+      terminal = vscode.window.createTerminal("[Mocha Test Terminal]");
+    }
+    terminal.show();
+    terminal.sendText(`cd ${filePath}..\\..`);
+    terminal.sendText(
+      `gulp build; mocha -g '${testName}' ${testFileName} --exit`
+    );
+  });
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
